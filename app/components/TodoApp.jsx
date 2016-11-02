12643b1595263175524e47a31280826040752893
ploidt <ploid@Ploid-Macbook.local>
2016-11-02 21:25:27 +0700

    add static todo lost

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'play with Paopao'
+        },
+        {
+          id: 2,
+          text: 'wash the dish'
+        },
+        {
+          id: 3,
+          text: 'penguin pu'
+        },
+        {
+          id: 4,
+          text: 'resume'
+        }
+      ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     );
   }
