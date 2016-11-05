c4204f5d666aa072d60e8af95908825604eeb4e6
ploidt <ploid@Ploid-Macbook.local>
2016-11-05 22:31:57 +0700

    Add TodoAPI

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,34 +1,21 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
       showCompleted: false,
       searchText: '',
-      todos: [
-        {
-          id: uuid(),
-          text: 'play with Paopao',
-          completed: false
-        },{
-          id: uuid(),
-          text: 'wash the dish',
-          completed: true
-        },{
-          id: uuid(),
-          text: 'penguin pu',
-          completed: true
-        },{
-          id: uuid(),
-          text: 'resume',
-          completed: false
-        }
-      ]
+      todos: TodoAPI.getTodos()
     };
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
   },
   handleAddTodo: function (text) {
     this.setState({
