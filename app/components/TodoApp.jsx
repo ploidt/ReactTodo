52429e513eb0ff7476723ba5cfd8d2c3f50fed5b
ploidt <ploid@Ploid-Macbook.local>
2016-11-05 17:25:37 +0700

    Complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -10,26 +11,31 @@ var TodoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'play with Paopao'
-        },
-        {
-          id: 2,
+        },{
+          id: uuid(),
           text: 'wash the dish'
-        },
-        {
-          id: 3,
+        },{
+          id: uuid(),
           text: 'penguin pu'
-        },
-        {
-          id: 4,
+        },{
+          id: uuid(),
           text: 'resume'
         }
       ]
     };
   },
   handleAddTodo: function (text) {
-    alert('new todo: ' + text);
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    });
   },
   handleSearch: function (showCompleted, searchText){
     this.setState({
